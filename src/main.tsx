82066a921c33e865585a303f7b7706bda324aa23
Niklas Fähnrich <niklas.fahnrich@safeture.com>
2025-09-01 09:33:56 +0200

    create three specific routes to three specific pokemons

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,19 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.css';
+import { createRoot } from 'react-dom/client';
 import { App } from './components/App.tsx';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import { PokemonView } from './views/PokemonView.tsx';
+import { pokemons } from './pokemons.ts';
 
+// Declarative Router
 createRoot(document.getElementById('root')!).render(
-    <App />
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/pikachu" element={<PokemonView pokemon={pokemons.pikachu} />} />
+      <Route path="/charizard" element={<PokemonView pokemon={pokemons.charizard} />} />
+      <Route path="/dragonite" element={<PokemonView pokemon={pokemons.dragonite} />} />
+    
+    </Routes>
+  </BrowserRouter>
+);
